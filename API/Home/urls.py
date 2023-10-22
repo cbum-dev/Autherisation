298d7393a95a169656d9422786cf518f566da0ed
Priyanshu200223 <priyanshukumar2002234@gmail.com>
2023-10-22 19:18:32 +0530

    API

---
+++ API/Home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from Home import views
+
+
+urlpatterns = [
+    path('',views.home,name="home"),
+    path('post-todo' , views.post_todo,name = 'post_todo')
+
+]
---
+++ API/Home/views.py
@@ -0,0 +1,37 @@
+# from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# Create your views here.
+from .serializer import TodoSerializer
+@api_view(['GET','POST'])
+def home(request):
+        if request.method == 'GET':
+            return Response({
+                'status' : 'GET',
+                'messg' : 'working'
+                  
+            })
+        elif request.method == 'POST':
+              return Response({
+                    'status' : 'POST',
+                    'message' : 'working'
+              })
+
+
+@api_view(['POST'])
+def post_todo(request):
+    try:
+        data = request.data 
+        print(data)
+        return Response({
+             'status' : True,
+             'message' : 'success!!!!!!!!!'
+        })
+    except Exception as e:
+        print(e)
+        return Response({
+             'status' : True,
+             'mmessage' : 'hahaha'
+        })
+        
+         
