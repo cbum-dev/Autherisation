44f65e0d041bcfa36cf8087d8110865936efaa56
Priyanshu200223 <priyanshukumar2002234@gmail.com>
2023-10-26 12:31:05 +0530

    models integrated

--- pyapi/app/main.py
+++ pyapi/app/main.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI,status,Response,HTTPException
+from fastapi import FastAPI,status,Response,HTTPException,Depends
 from typing import Optional
 from fastapi.params import Body #to display output in body of terminal .
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
+from sqlalchemy.orm import Session
 from psycopg2.extras import RealDictCursor
+from . import models
+from .database import engine,get_db
 
-
+models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
+
+
 
 my_post = [{"title":"Dhoom","content":"nothing","id":1},
            {"title":"Dhoom2","content":"nothing2","id":2}
@@ -38,7 +43,7 @@ while True:
 
 @app.get("/")
 def read_root():
-    cursor.execute(""" SELECT * FROM post""")
+    cursor.execute(""" SELECT * FROM post ORDER BY title""")
     posts = cursor.fetchall()
     return {"data":posts}
     # return {"Hello": my_post}
@@ -47,34 +52,47 @@ def read_root():
 # def create(payLoad: dict = Body(...)): # created a ducntion with variable payload and with dictory bin the body.
 #     print(payLoad)
 #     return {"message" : f"title{payLoad['todo_name']} content{'todo_description'}"}
+# def create_post(post : Post):
+#     # print(post)                     
+#     # print(post.dict())
+#     post_dict = post.dict()
+#     post_dict['id'] = randrange(0,1999999)
+#     my_post.append(post_dict)
 def create_post(post : Post):
-    # print(post)                     
-    # print(post.dict())
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0,1999999)
-    my_post.append(post_dict)
+    cursor.execute(""" INSERT INTO post(title,content,published) VALUES
+                   (%s,%s,%s) RETURNING * """,(post.title,post.content,post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data" : "created SUCCESSFULLY: ","post" : new_post}
     
-    return {"data" : post_dict}
+    # return {"data" : post_dict}
 
 # @app.get("/post/{id}")
 # def get_post(id : int , response : Response):
 #     post = find_post
 @app.delete("/post/{id}" , status_code=status.HTTP_204_NO_CONTENT )
 def delete_post(id : int):
-    index = find_index(id)
-    if index == None:
+    cursor.execute("""DELETE FROM post where id = %s RETURNING *""", (str(id),))
+    deleted = cursor.fetchone()
+    conn.commit()
+    if deleted == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with this {id} doesn't exist")
-    my_post.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/post/{id}")
 def update_post(id : int, post : Post):
-    index = find_index(id)
-    if index == None:
+    cursor.execute("""UPDATE post SET title = %s, content = %s, published = %s WHERE id = %s 
+                   RETURNING *""" ,(post.title,post.content,post.published,str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with this {id} doesn't exist")
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_post[index] = post_dict
-    return {"data": post_dict}
+
+    return {"data": updated_post}
+
+@app.get("/sql")
+def test_post(db : Session = Depends(get_db)):
+    posts = db.query(models.Posts).all()
+    return {"data" : posts}
